aaaf46f20e1bfbaf95ae63756bc459c5c061ef6a
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2022-06-09 17:32:56 -0400

    add functionality to component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,8 +2,19 @@ import './Home.css'
 
 import React from 'react'
 
+import { useFetch } from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
+
 export default function Home() {
+  const url = 'http://localhost:3000/recipes'
+  const { data: recipes, isLoading, error } = useFetch(url)
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      {isLoading && <h1 className='loading'>Recipes are loading...</h1>}
+      {error && <h1 className='error'>The following error occured: {error}</h1>}
+
+      {recipes && <RecipeList recipes={recipes} />}
+    </div >
   )
 }
