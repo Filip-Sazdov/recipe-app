5c8f334ee2b8475aeb7830236ede508d198eb7b3
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2022-06-10 15:23:25 -0400

    remove redundant code

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -10,12 +10,9 @@ export default function RecipeList({ recipes }) {
         <div key={recipe.id} className='card'>
           <h3>{recipe.title}</h3>
           <p>Cooking time: {recipe.cookingTime}</p>
-          {/* <p>Ingredients:</p>
-          <p>{recipe.ingredients}</p> */}
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
         </div>
-
       ))}
     </div>
   )
